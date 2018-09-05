94c139fb2bb8c08e7eff9057fb8a894f5f3bd065
unknown <isabelabatarce15@gmail.com>
2018-09-05 00:51:59 -0300

    Finalizado, falta apenas o botão e os espaçamentos

--- startbootstrap-agency-gh-pages/js/category.js
+++ startbootstrap-agency-gh-pages/js/category.js
@@ -1,17 +1,37 @@
 
 var server = document.getElementById("server");
 var design = document.getElementById("design");
-var programming = document.getElementById("programmingbtn");
-
+var programming = document.getElementById("programming");
+var portfolio = document.getElementById("portfolio");
 function hide() {
-	 document.getElementById("portfolio").style.display = "none";
+	 document.getElementById("design").style.display = "none";
+	 document.getElementById("server").style.display = "none";
+	 document.getElementById("programming").style.display = "none";	
+
 }
 
-function displayFunction() {
+function displayFunction_design() {
  
  if (design.style.display === 'none') {
  design.style.display = 'block';
  } else {
  design.style.display = 'none';
+ } 
+}
+
+function displayFunction_server() {
+ 
+ if(server.style.display ==='none'){
+ 	server.style.display ='block';
+ }else{
+ 	server.style.display = 'none';
+ }
+}
+function displayFunction_programming() {
+ 
+ if (programming.style.display === 'none') {
+ programming.style.display = 'block';
+ } else {
+ programming.style.display = 'none';
  }
 }
